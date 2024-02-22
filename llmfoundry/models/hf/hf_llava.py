4cd30ae0062376efcbb83a0f97cfb9f5a364d45a
jazcollins <jasmine@mosaicml.com>
2024-02-22 00:45:05 +0000

    rm gradient checkpointing bugfix - works fine for transformers 4.37.2

--- llmfoundry/models/hf/hf_llava.py
+++ llmfoundry/models/hf/hf_llava.py
@@ -115,12 +115,6 @@ class ComposerHFLLaVa(HuggingFaceModel):
             # Add LLaVA special tokens to tokenizer
             tokenizer.add_tokens(['<image>', '<pad>'], special_tokens=True)
 
-        # Fix this issue - https://github.com/huggingface/transformers/issues/26969
-        #   File "/usr/lib/python3/dist-packages/transformers/models/mistral/modeling_mistral.py", line 293, in forward
-        # raise ValueError(
-        # ValueError: Attention mask should be of size (1, 1, 1599, 3198), but is torch.Size([1, 1, 1599, 1599]    
-        model.language_model.gradient_checkpointing_enable(gradient_checkpointing_kwargs={'use_reentrant': True})
-
         # Fix weights for current phase of training
         model.vision_tower.requires_grad_(False)
         if not om_model_config.train_llm:
